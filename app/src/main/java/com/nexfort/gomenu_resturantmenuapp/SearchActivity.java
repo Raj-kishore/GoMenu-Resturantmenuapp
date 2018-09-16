c2c0b2a08c8da3b2d5efe90bd7b5d5ac59de067d
Raj Maharana <raj.maharana.94@gmail.com>
2018-09-17 00:53:43 +0530

    Initial commit

---
+++ app/src/main/java/com/nexfort/gomenu_resturantmenuapp/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.nexfort.gomenu_resturantmenuapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rj kShr on 19-08-2018.
+ */
+
+public class SearchActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/nexfort/gomenu_resturantmenuapp/SettingActivity.java
@@ -0,0 +1,8 @@
+package com.nexfort.gomenu_resturantmenuapp;
+
+/**
+ * Created by Rj kShr on 19-08-2018.
+ */
+
+public class SettingActivity {
+}
